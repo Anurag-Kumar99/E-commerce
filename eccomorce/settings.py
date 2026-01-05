37d8bb98e2c0bb5e017991798f7eb8e8d323bd2e
Anuraj-Kumar99 <anurag995539@gmail.com>
2026-01-05 10:50:18 +0530

    fix csrf trusted origins for railway domain

--- eccomorce/settings.py
+++ eccomorce/settings.py
@@ -134,3 +134,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://e-commerce-prod-test.up.railway.app"
+]
+
