dd2ae62806d7731b4a5ff21640a197cf52b1476c
Anuraj-Kumar99 <anurag995539@gmail.com>
2026-01-05 11:28:13 +0530

    Configure WhiteNoise for static files

--- eccomorce/settings.py
+++ eccomorce/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # mainaging media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
